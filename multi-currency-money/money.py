69ba39d132c09387fe1cb955a89c06822d183ecd
zerebom <ice.choco.pudding.kokoro@gmail.com>
2020-10-12 07:35:02 +0900

    Finish ch08

--- multi-currency-money/money.py
+++ multi-currency-money/money.py
@@ -2,7 +2,23 @@ class Money:
     def __init__(self, amount: int) -> None:
         self.amount = amount
 
+    def __eq__(self, obj):
+        # Pythonの等価性はこれでかける
+        return (self.amount == obj.amount) & (self.__class__ == obj.__class__)
 
-    def equals(self, object):
-        money = object
-        return (self.amount == money.amount) & (self.__class__ == money.__class__)
+    @staticmethod
+    def dollor(amount:int):
+        #Factory Method
+        return Dollor(amount)
+
+    @staticmethod
+    def franc(amount:int):
+        return Franc(amount)
+
+class Franc(Money):
+    def times(self, multiplier):
+        return Franc(self.amount * multiplier)
+
+class Dollor(Money):
+    def times(self, multiplier):
+        return Dollor(self.amount * multiplier)
--- multi-currency-money/money_test.py
+++ multi-currency-money/money_test.py
@@ -1,23 +1,23 @@
 import unittest
 
-from dollar import Dollor,Franc
+from money import Dollor,Franc,Money
 
 class MoneyTest(unittest.TestCase):
     def test_multiplication(self):
-        five = Dollor(5)
-        self.assertTrue(Dollor(10).equals(five.times(2)))
-        self.assertTrue(Dollor(15).equals(five.times(3)))
+        five = Money.dollor(5)
+        self.assertTrue(Dollor(10) == (five.times(2)))
+        self.assertTrue(Dollor(15) == (five.times(3)))
 
     def test_franc_multiplication(self):
-        five = Franc(5)
-        self.assertTrue(Franc(10).equals(five.times(2)))
-        self.assertTrue(Franc(15).equals(five.times(3)))
+        five = Money.franc(5)
+        self.assertTrue(Franc(10) == (five.times(2)))
+        self.assertTrue(Franc(15) == (five.times(3)))
 
     def test_equality(self):
         # Value Objectにするため、等価性のテストを行う
-        self.assertTrue(Dollor(5).equals(Dollor(5)))
-        self.assertFalse(Dollor(5).equals(Dollor(6)))
-        self.assertFalse(Dollor(5).equals(Franc(5)))
+        self.assertTrue(Dollor(5) == (Dollor(5)))
+        self.assertFalse(Dollor(5) == (Dollor(6)))
+        self.assertFalse(Dollor(5) == (Franc(5)))
 
 if __name__ == "__main__":
     unittest.main()
