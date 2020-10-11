71de80d55b45e2ba8cc6b4253bafeeb8a81c770e
zerebom <ice.choco.pudding.kokoro@gmail.com>
2020-10-12 08:33:06 +0900

    Finish ch09

--- multi-currency-money/money.py
+++ multi-currency-money/money.py
@@ -1,24 +1,37 @@
 class Money:
-    def __init__(self, amount: int) -> None:
+    def __init__(self, amount: int, currency: str) -> None:
         self.amount = amount
+        self._currency = currency
 
     def __eq__(self, obj):
         # Pythonの等価性はこれでかける
         return (self.amount == obj.amount) & (self.__class__ == obj.__class__)
 
     @staticmethod
-    def dollor(amount:int):
-        #Factory Method
-        return Dollor(amount)
+    def dollor(amount: int):
+        # Factory Method
+        return Dollor(amount,"USD")
 
     @staticmethod
-    def franc(amount:int):
-        return Franc(amount)
+    def franc(amount: int):
+        return Franc(amount,"CHF")
+
+    def currency(self):
+        return self._currency
 
 class Franc(Money):
+    def __init__(self,amount,currency) -> None:
+        super().__init__(amount,currency)
+
     def times(self, multiplier):
-        return Franc(self.amount * multiplier)
+        return Money.franc(self.amount * multiplier)
+
+
 
 class Dollor(Money):
+    def __init__(self,amount,currency) -> None:
+        super().__init__(amount,currency)
+
     def times(self, multiplier):
-        return Dollor(self.amount * multiplier)
+        return Money.dollor(self.amount * multiplier)
+
--- multi-currency-money/money_test.py
+++ multi-currency-money/money_test.py
@@ -5,19 +5,23 @@ from money import Dollor,Franc,Money
 class MoneyTest(unittest.TestCase):
     def test_multiplication(self):
         five = Money.dollor(5)
-        self.assertTrue(Dollor(10) == (five.times(2)))
-        self.assertTrue(Dollor(15) == (five.times(3)))
+        self.assertTrue(Money.dollor(10) == (five.times(2)))
+        self.assertTrue(Money.dollor(15) == (five.times(3)))
 
     def test_franc_multiplication(self):
         five = Money.franc(5)
-        self.assertTrue(Franc(10) == (five.times(2)))
-        self.assertTrue(Franc(15) == (five.times(3)))
+        self.assertTrue(Money.franc(10) == (five.times(2)))
+        self.assertTrue(Money.franc(15) == (five.times(3)))
 
     def test_equality(self):
         # Value Objectにするため、等価性のテストを行う
-        self.assertTrue(Dollor(5) == (Dollor(5)))
-        self.assertFalse(Dollor(5) == (Dollor(6)))
-        self.assertFalse(Dollor(5) == (Franc(5)))
+        self.assertTrue(Money.dollor(5) == (Money.dollor(5)))
+        self.assertFalse(Money.dollor(5) == (Money.dollor(6)))
+        self.assertFalse(Money.dollor(5) == (Money.franc(5)))
+
+    def test_currency(self):
+        self.assertEqual("USD", Money.dollor(1).currency())
+        self.assertEqual("CHF", Money.franc(1).currency())
 
 if __name__ == "__main__":
     unittest.main()
